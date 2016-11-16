4a5c140ae2d3989ce9eedbbfef55fa9c2ddbfe72
Jeff <jeff_m_england@yahoo.com>
2016-11-16 19:42:45 +0000

    changed port functionality

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/", function (req, res) {
   });                                                
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log("App listening on port 8080!");
 });
